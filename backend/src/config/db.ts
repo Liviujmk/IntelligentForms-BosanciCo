834f69817e0e767ec06982d7659c37561cc71f89
Lucian Mitrofan <lucmit214@gmail.com>
2023-02-27 18:02:17 +0200

    First commit! Added a base code for application backend, typescript + express

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@
 
 import mongoose from 'mongoose';
 require('dotenv').config();
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/DB_NAME';
+const MONGO_URI = process.env.MONGO_URI || '';
 
 mongoose.connect(MONGO_URI);
 
