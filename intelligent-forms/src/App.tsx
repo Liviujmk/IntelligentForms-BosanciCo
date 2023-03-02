a5027e1ca06a32c4a1fb2a3a3f04c1f2a3de8f81
admin <admin@DESKTOP-6D3IIJS>
2023-03-02 20:18:46 +0200

    created react app

--- intelligent-forms/src/App.tsx
+++ intelligent-forms/src/App.tsx
@@ -15,13 +15,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello World</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          edited <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
