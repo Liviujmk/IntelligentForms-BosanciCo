66858b9e7d42a60b43f0ab12ce7eab9ac924af7a
Lucian Mitrofan <lucmit214@gmail.com>
2023-03-04 16:27:53 +0200

    Added basic routes

---
+++ intelligent-forms/src/features/auth/AuthProvider.tsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({}); //create context
+export const AuthProvider = ({ children }: any) => {
+    const [auth, setAuth] = useState({});
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
