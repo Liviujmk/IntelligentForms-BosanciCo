d447f65acf73e8d825288ff305e3dd2adb38df64
Lucian Mitrofan <lucmit214@gmail.com>
2023-03-17 17:47:34 +0200

    Fixed build bug

--- intelligent-forms/src/features/submissioons/api/api.submissions.ts
+++ intelligent-forms/src/features/submissioons/api/api.submissions.ts
@@ -1,16 +1,16 @@
 import { API_PATH_PROD } from "../../../config/api";
-import Submission from "../types/submission.types";
+// import Submission from "../types/submission.types";
 
-export const getFormRecognizerData = async (formId: string) => {
-    try {
-        const response = await fetch(`${API_PATH_PROD}fill/forms/${formId}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        return response.json();
-    } catch (error) {
-        return {error : error}
-    }
-};
+// export const getFormRecognizerData = async (formId: string) => {
+//     try {
+//         const response = await fetch(`${API_PATH_PROD}fill/forms/${formId}`, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             }
+//         });
+//         return response.json();
+//     } catch (error) {
+//         return {error : error}
+//     }
+// };
