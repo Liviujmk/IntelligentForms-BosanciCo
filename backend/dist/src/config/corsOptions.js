60d350dc6c87153a06a6cb565268fe654ba5e149
Lucian Mitrofan <lucmit214@gmail.com>
2023-09-05 14:02:13 +0300

    added build

--- backend/dist/src/config/corsOptions.js
+++ backend/dist/src/config/corsOptions.js
@@ -10,15 +10,8 @@ const allowedOrigins = [
 ];
 exports.allowedOrigins = allowedOrigins;
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: "*",
     optionsSuccessStatus: 200,
-    credentials: true
+    // credentials: true
 };
 exports.corsOptions = corsOptions;
--- backend/dist/src/controllers/auth.controller.js
+++ backend/dist/src/controllers/auth.controller.js
@@ -57,11 +57,11 @@ const authController = {
 };
 exports.authController = authController;
 const isAuthenticated = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(req.cookies);
-    if (req.cookies.access_token) {
-        const { access_token } = req.cookies;
+    console.log(req.headers);
+    if (req.headers.authorization) {
+        const { authorization } = req.headers;
         //find user by access token
-        const foundUser = yield user_1.default.findOne({ access_token }).exec();
+        const foundUser = yield user_1.default.findOne({ access_token: authorization }).exec();
         if (foundUser) {
             // @ts-ignore
             req.userId = foundUser.id;
